0503e56bfc2cd29f5eac3b6f1814f7250ad54905
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2021-07-12 15:48:59 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.myapp.databinding.ActivityMainBinding
+import com.example.myapp.fragments.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    val catalogFragment = CatalogFragment()
+    val profileFragment = ProfileFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        makeCurrentFragment(catalogFragment)
+        binding.bottomNavigationMenu.selectedItemId = R.id.miCatalog
+        binding.bottomNavigationMenu.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.miCatalog -> makeCurrentFragment(catalogFragment)
+                R.id.miProfile -> makeCurrentFragment(profileFragment)
+                R.id.miSettings -> {}
+            }
+            true
+        }
+
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+}
