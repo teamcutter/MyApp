da66b250f2cc092d6c612ba435be860ac382f7ac
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2021-07-18 23:13:33 +0600

    added CardItem.kt dataclass, optimized RecyclerAdapter.kt for this dataclass, added nullable check

--- app/src/main/java/com/example/myapp/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/myapp/adapter/RecyclerAdapter.kt
@@ -8,18 +8,21 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
 import com.example.myapp.R
+import com.example.myapp.dataclasses.CardItem
 
 class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    private var titles = arrayOf("Food", "Clothes", "Desktop", "Laptop", "Phone", "Other", "Car", "Trip", "Booking", "Repair")
-    private val images = intArrayOf(
-        R.drawable.ic_diet, R.drawable.ic_clothes,
-        R.drawable.ic_desktop, R.drawable.ic_laptop,
-        R.drawable.ic_phone, R.drawable.ic_vacuum_cleaner,
-        R.drawable.ic_car, R.drawable.ic_bay,
-        R.drawable.ic_hotel, R.drawable.ic_repair_tools)
+    val cardItem: CardItem = CardItem(
+        arrayListOf(
+            "Food", "Clothes", "Desktop", "Laptop", "Phone",
+            "Other", "Car", "Trip", "Booking", "Tools"),
+        arrayListOf(
+            R.drawable.ic_diet, R.drawable.ic_clothes,
+            R.drawable.ic_desktop, R.drawable.ic_laptop,
+            R.drawable.ic_phone, R.drawable.ic_vacuum_cleaner,
+            R.drawable.ic_car, R.drawable.ic_bay,
+            R.drawable.ic_hotel, R.drawable.ic_repair_tools))
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.cardveiw_item_category, parent, false)
@@ -27,12 +30,12 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
-        holder.itemTextView.text = titles[position]
-        holder.itemImageView.setImageResource(images[position])
+        holder.itemTextView.text = cardItem.titles[position]
+        cardItem.images[position]?.let { holder.itemImageView.setImageResource(it) }
     }
 
     override fun getItemCount(): Int {
-        return titles.size
+        return cardItem.titles.size
     }
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
@@ -47,7 +50,7 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
             itemImageButton.setOnClickListener {
                 val position: Int = adapterPosition
-                Toast.makeText(itemView.context, "clicked on ${titles[position]}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(itemView.context, "clicked on ${cardItem.titles[position]}", Toast.LENGTH_SHORT).show()
             }
 
         }
